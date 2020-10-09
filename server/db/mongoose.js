23b3e5a6bc6fed168ea436bb7aa0a70551697a34
OmarGmzRmz <gomar8138@gmail.com>
2020-10-08 19:41:02 -0500

    Secured Datbase Connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose');
-
 mongoose.Promise = global.Promise;
-
 const HOST = 'localhost:27017';
 const DATABASE = 'TodoApp';
-
-mongoose.connect(`mongodb://nodejs-todo-app-no-auth:0TKxQMyxiL5h35fIjckKbWjsRDWX2ftZojizGT2MsZ6nqDuP7qZXvQcE9o8mXktR4OHkczfSiGRAUWpCnUZFLg%3D%3D@nodejs-todo-app-no-auth.mongo.cosmos.azure.com:10255/?ssl=true&appName=@nodejs-todo-app-no-auth@`);
-
-
+const connectionString = process.env.MONGODB_URI || `mongodb://${HOST}/${DATABASE}`;
+mongoose.connect(connectionString);
 module.exports = {mongoose};
